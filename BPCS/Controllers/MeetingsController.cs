d2d7e992ffb76cc18bccd6b03d077c9696e29f64
John Proctor <jceproctor@googlemail.com>
2016-08-29 09:08:49 +0100

    Add meeting dates for 2016

---
+++ BPCS/Controllers/MeetingsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BPCS.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
